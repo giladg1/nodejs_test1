6f6658d34ddf619c1cadd225045a0186722894aa
gilad <gilad.glasberg@gmail.com>
2016-05-24 15:37:43 +0300

    renamed files name and change package.json main file name to index.js

--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,4 @@
-var checkUrl = require('./checkUrlModule.js');
+var checkUrl = require('./index.js');
 
 // test the url parser module
 var ourObject = checkUrl.parseURL('http://www.node.org/docs/index.html');
